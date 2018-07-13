b5b68567ec868f590c1e8f8385e49ba7feb1edfe
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-07-13 17:28:40 +0400

    Added MainView.xaml, some dictionaries and converter class

--- Monify/Monify/Views/MainView.xaml.cs
+++ Monify/Monify/Views/MainView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Monify.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Monify
+namespace Monify.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : UserControl
     {
-        MainView mainView;
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
-            mainView = new MainView();
-            MainGrid.Children.Add(mainView);
         }
     }
 }
