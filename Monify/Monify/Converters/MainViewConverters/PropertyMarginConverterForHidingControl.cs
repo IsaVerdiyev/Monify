f0713888f531119ff0ad112da7400d85ff0ddc97
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-07-18 12:27:19 +0400

    Deleted unused converter HeightToMaximumHeightConverter; Added new converter for hiding side menus by binding margin to their actualwidth

---
+++ Monify/Monify/Converters/MainViewConverters/PropertyMarginConverterForHidingControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Monify.Converters.MainViewConverters
+{
+
+    class PropertyMarginConverterForHidingControl : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double obj = (double)value;
+            string par = parameter as string;
+            if (par != null)
+            {
+                if (par.ToLower() == "left")
+                {
+                    return new Thickness(-obj, 0, 0, 0);
+                }
+                else if (par.ToLower() == "top")
+                {
+                    return new Thickness(0, -obj, 0, 0);
+                }
+                else if (par.ToLower() == "right")
+                {
+                    return new Thickness(0, 0, -obj, 0);
+                }
+                else if (par.ToLower() == "bottom")
+                {
+                    return new Thickness(0, 0, 0, -obj);
+                }
+                else
+                {
+                    return new Exception("Invalid parameter passed in PropertyMarginConverter");
+                }
+            }
+            else
+            {
+                return new Exception("Invalid parameter passed in PropertyMarginConverter");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
