ec33691040d14098223708ce840d24e4561f8d30
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-07-19 17:28:29 +0400

    Added CalculatorView usercontrol and included it in CostProfitCalculatorView and TransactionCalculatorview. Also tried to implement some commands for making calculator work in AbsractCalculatorViewModel but yet it doesn't work

--- Monify/Monify/ViewModels/AbstractCalculatorViewModel.cs
+++ Monify/Monify/ViewModels/AbstractCalculatorViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Monify.Tools;
+﻿using Monify.Services;
+using Monify.Tools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,54 @@ using System.Threading.Tasks;
 
 namespace Monify.ViewModels
 {
+
     abstract class AbstractCalculatorViewModel: ObservableObject,  IViewModel
     {
-        public IViewModel ResetToInitialState()
-        {
-            return this;
-        }
-
 
         decimal amount;
 
         public Decimal Amount { get => amount; set => SetProperty(ref amount, value); }
+
+        string textBoxNumber;
+
+        public string TextBoxNumber { get => textBoxNumber; set => SetProperty(ref textBoxNumber, value); }
+
+        CalculatorOperations? chosenOperation;
+
+        private RelayCommand calculatorNumberButtonClickCommand;
+
+        public RelayCommand CalculatorNumberButtonClickCommand
+        {
+            get {
+                return calculatorNumberButtonClickCommand ??
+                    (calculatorNumberButtonClickCommand = new RelayCommand(obj =>
+                    {
+                        string objText = obj as string;
+                        if(Double.TryParse(objText, out double res))
+                        {
+                            TextBoxNumber += objText;
+                        }
+                    }));
+            }
+        }
+
+        private RelayCommand calculatorArithmeticOperationButtonCommand;
+
+        public RelayCommand CalculatorArithmeticOperationButtonCommand
+        {
+            get {
+                return calculatorArithmeticOperationButtonCommand ??
+                    (calculatorArithmeticOperationButtonCommand = new RelayCommand(obj =>
+                    {
+                       
+                    }));
+            }
+        }
+
+
+        public IViewModel ResetToInitialState()
+        {
+            return this;
+        }
     }
 }
