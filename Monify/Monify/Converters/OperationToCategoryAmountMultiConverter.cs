5cc7a69131def1474cfffbb268d0f1b4359c4c85
IsaVerdiyev <37246016+IsaVerdiyev@users.noreply.github.com>
2018-07-26 19:07:59 +0400

    Created MultiBinding class 'OperationToCategoryAmountMultiConverter'. There is some error yet - need to correct

---
+++ Monify/Monify/Converters/OperationToCategoryAmountMultiConverter.cs
@@ -0,0 +1,29 @@
+﻿using Monify.Models;
+using Monify.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Monify.Converters
+{
+    class OperationToCategoryAmountMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Operation operation = values[0] as Operation;
+
+            IStorage storage = values[1] as IStorage;
+
+            return $"{storage.OperationCategories.FirstOrDefault(cat => cat.Index == operation?.OperationCategoryIndex)} - {operation?.Amount}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
